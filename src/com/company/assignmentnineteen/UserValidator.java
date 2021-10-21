4d4bdf8ffe16d43f13a99298794b3b58f2037712
Sam-19091994 <jadhavdevendra40@gmail.com>
2021-10-21 06:11:46 +0530

    Added: Checking If The Email Entered By The User Is Vaid

--- src/com/company/assignmentnineteen/UserValidator.java
+++ src/com/company/assignmentnineteen/UserValidator.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserValidator {
     private static final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$"; //regex
     private static final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$"; //regex
+    private static final String emailIDPattern = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$"; //regex
 
     public boolean validateFirstName(String fName) {
         Pattern pattern = Pattern.compile(firstNamePattern);
@@ -16,11 +17,18 @@ public class UserValidator {
         return pattern2.matcher(lName).matches();
     }
 
+    public boolean validateEmailAddress(String email) {
+        Pattern pattern3 = Pattern.compile(emailIDPattern);
+        return pattern3.matcher(email).matches();
+    }
+
     public static void main(String[] args) {
         UserValidator validator = new UserValidator();
         boolean result = validator.validateFirstName("Sameer");
         boolean resultLastName = validator.validateLastName("Jadhav");
+        boolean resultEmail = validator.validateEmailAddress("abc+100@yahoo.com");
         System.out.println(result);
         System.out.println(resultLastName);
+        System.out.println(resultEmail);
     }
 }
