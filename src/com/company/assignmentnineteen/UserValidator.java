f7fe2fdfe1400eef4d61792733f9445a73c2b011
Sam-19091994 <jadhavdevendra40@gmail.com>
2021-10-21 06:19:13 +0530

    Added: Checking If The Mobile Number Entered By The User Is Valid

--- src/com/company/assignmentnineteen/UserValidator.java
+++ src/com/company/assignmentnineteen/UserValidator.java
@@ -6,6 +6,7 @@ public class UserValidator {
     private static final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$"; //regex
     private static final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$"; //regex
     private static final String emailIDPattern = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$"; //regex
+    private static final String mobileNumberPattern = "(0|91)*[ ][7-9][0-9]{9}"; //regex
 
     public boolean validateFirstName(String fName) {
         Pattern pattern = Pattern.compile(firstNamePattern);
@@ -22,13 +23,20 @@ public class UserValidator {
         return pattern3.matcher(email).matches();
     }
 
+    public boolean validateMobileNumber(String mobileNumber) {
+        Pattern pattern3 = Pattern.compile(mobileNumberPattern);
+        return pattern3.matcher(mobileNumber).matches();
+    }
+
     public static void main(String[] args) {
         UserValidator validator = new UserValidator();
         boolean result = validator.validateFirstName("Sameer");
         boolean resultLastName = validator.validateLastName("Jadhav");
         boolean resultEmail = validator.validateEmailAddress("abc+100@yahoo.com");
+        boolean resultMobileNumber = validator.validateMobileNumber("0 7568438745");
         System.out.println(result);
         System.out.println(resultLastName);
         System.out.println(resultEmail);
+        System.out.println(resultMobileNumber);
     }
 }
